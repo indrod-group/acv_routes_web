4b2beb7ff2ce2c2e894b64c52160536990964bed
Christopher Villamarín <alexpila.314@gmail.com>
2023-12-18 01:20:27 -0500

    docs: Document useRoutePositions hook and its functions

--- src/api/hooks/useRoutePositions.ts
+++ src/api/hooks/useRoutePositions.ts
@@ -1,38 +1,42 @@
 import { useState, useEffect } from 'react';
-import useToken from './useToken';
-import { Convert } from '../Conversor';
-import { Route } from '../models/Route';
-import axios from 'axios';
 import { message } from 'antd';
 
+import { Convert } from '../Conversor';
+import { Route } from '../models/Route';
 import useProfile from './useProfile';
 import type { UserProfile } from '../models/UserProfile';
+import useApi from './useApi';
 
-export function useRoutePositions() {
-  const { token } = useToken();
-  const { userProfile } = useProfile() as { userProfile: UserProfile };
-
+/**
+ * useRoutePositions hook
+ * 
+ * This hook is used to fetch and handle the route positions associated with a specific user profile.
+ * 
+ * @returns {Object} The state and functions of the hook.
+ * @returns {Route[] | undefined} .routePositions - The fetched route positions.
+ * @returns {() => void} .refreshRoutePositions - The function to refresh the route positions.
+ */
+export function useRoutePositions(): { routePositions: Route[] | undefined, refreshRoutePositions: () => void } {
   const [routePositions, setRoutePositions] = useState<Route[]>();
+  const { userProfile } = useProfile() as { userProfile: UserProfile };
+  const api = useApi();
 
   useEffect(() => {
     if (!userProfile?.uuid) {
       console.error('No user profile UUID found');
       return;
     }
-    axios.get(`https://twlxb59c-9090.use2.devtunnels.ms/api/v1/user-routes/${userProfile.uuid}/`, {
-      headers: {
-        Authorization: `Token ${token?.token as string}`,
-      },
-    })
+    api.get(`/user-routes/${userProfile.uuid}/`)
       .then(response => {
         setRoutePositions(Convert.toRoutes(JSON.stringify(response.data)));
+        void message.info("Las rutas y sus posiciones se han obtenido correctamente.");
       })
       .catch(error => {
-        void message.error("No se han podido mostrar las rutas y sus posiciones");
         console.error(error);
+        void message.error("Ha habido un error obteniendo las rutas y sus posiciones.");
       });
-  }, [token, userProfile]);
-
+  }, [api, userProfile]);
+ 
   return {
     routePositions,
     refreshRoutePositions: () => setRoutePositions(undefined),
