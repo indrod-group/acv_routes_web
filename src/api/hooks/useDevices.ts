2f56a511f58dd347fd523b8c6e1de144f428b236
Christopher Villamarín <alexpila.314@gmail.com>
2023-12-18 00:48:37 -0500

    docs: Document useDevices hook and its functions

--- src/api/hooks/useDevices.ts
+++ src/api/hooks/useDevices.ts
@@ -1,42 +1,55 @@
 import { useState, useEffect, useCallback } from 'react';
-import axios from 'axios';
-import useToken from './useToken';
+import { message } from 'antd';
+
 import { Device } from '../models/Device';
 import { Convert } from '../Conversor';
 import useProfile from './useProfile';
 import type { UserProfile } from '../models/UserProfile';
+import useApi from './useApi';
 
-export function useDevices() {
-  const { token } = useToken();
+/**
+ * useDevices hook
+ * 
+ * This hook is used to fetch and handle devices associated with a specific user profile.
+ * 
+ * @returns {Object} The state and functions of the hook.
+ * @returns {Device[]} .devices - The fetched devices, sorted by user name.
+ * @returns {() => void} .fetchDevices - The function to fetch devices.
+ */
+export function useDevices(): { devices: Device[], fetchDevices: () => void } {
   const { userProfile } = useProfile() as { userProfile: UserProfile };
   const [devices, setDevices] = useState<Device[]>([]);
+  const api = useApi();
 
+  /**
+   * fetchDevices
+   * 
+   * This function is used to fetch devices associated with the user profile.
+   * The devices are stored in the state.
+   * If an error occurs while fetching the devices, an error message is displayed.
+   */
   const fetchDevices = useCallback(() => {
     if (userProfile) {
-      axios.get(`https://twlxb59c-9090.use2.devtunnels.ms/api/v1/user-devices/${userProfile.uuid}/`, {
-        headers: {
-          'Authorization': `Token ${token!.token}`
-        },
-      })
+      api.get(`/user-devices/${userProfile.uuid}/`)
         .then(response => {
-          setDevices(Convert.toDevices(JSON.stringify(response.data)));
+          const devices = Convert.toDevices(JSON.stringify(response.data));
+          const sortedDevices = Array.from(devices).sort((a, b) => a.user_name.localeCompare(b.user_name));
+          setDevices(sortedDevices);
+          void message.info('Se han obtenido los dispositivos correctamente.');
         })
         .catch(error => {
-          console.error('Error fetching data: ', error);
+          console.error('Error fetching devices: ', error);
+          void message.error('Ha habido un problema obteniendo los dispositivos.');
         });
-    } else {
-      console.error('userProfile is undefined');
     }
-  }, [token, userProfile]);
+  }, [api, userProfile]);
 
   useEffect(() => {
     fetchDevices();
   }, [fetchDevices]);
 
-  const sortedData = Array.from(devices).sort((a, b) => a.user_name.localeCompare(b.user_name));
-
   return {
-    devices: sortedData,
+    devices: devices,
     fetchDevices: fetchDevices,
   };
 }
