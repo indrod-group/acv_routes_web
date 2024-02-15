7b2a34c6af714b97638020018c8203aea88554ac
Christopher Villamarín <alexpila.314@gmail.com>
2024-02-15 02:34:28 -0500

    fix: Build failed in useAlarms
    src/api/hooks/useAlarms.ts(1,20): error TS6133: 'useEffect' is declared but its value is never read.

--- src/api/hooks/useAlarms.ts
+++ src/api/hooks/useAlarms.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import { message } from 'antd';
 
 import { Alarm } from '../models/Alarm';
