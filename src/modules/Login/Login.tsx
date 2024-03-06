1b8be763e1902471a559a51ba861e17d9b05bdfa
Christopher Villamarín <alexpila.314@gmail.com>
2024-03-05 22:14:16 -0500

    feat(login): Add error handling and responsive design

--- src/modules/Login/Login.tsx
+++ src/modules/Login/Login.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import Cookies from 'js-cookie';
 
-import { Form, Input, Button, Layout, Card, Typography } from 'antd';
+import { Form, Input, Button, Layout, Card, Typography, message } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 import { UserToken } from '../../api/hooks/useToken'
@@ -26,6 +27,8 @@ async function loginUser(credentials: Credentials): Promise<UserToken> {
 }
 
 const LoginForm: React.FC<LoginProps> = ({ setToken }) => {
+  const isDesktopOrLaptop = useMediaQuery({ minWidth: 1000 });
+  const title = isDesktopOrLaptop ? 'Bitácora de conducción y sistema de administración de vehículos' : 'Road Safety System';
   const navigate = useNavigate();
 
   const onFinish = (values: Credentials) => {
@@ -33,15 +36,21 @@ const LoginForm: React.FC<LoginProps> = ({ setToken }) => {
       setToken(token);
       Cookies.set('username', values.username);
       navigate('/dashboard/journey-tracker');
-    }).catch(error => {
-      console.log(error);
+    }).catch((error: AxiosError) => {
+      if (!navigator.onLine) {
+        void message.error('No hay conexión a internet.');
+      } else if (error.response && error.response.status === 400) {
+        void message.error('Las credenciales son inválidas.');
+      } else {
+        void message.error('Ha habido un problema.')
+      }
     });
   }
 
   return (
     <Layout className="layout flex flex-col min-h-screen h-screen m-0 mx-auto">
       <Layout.Header className="header flex justify-center items-center">
-        <h1 className="text-white text-2xl">Bitácora de conducción y sistema de administración de vehículos </h1>
+        <h1 className="text-white text-2xl text-center py-5">{title}</h1>
       </Layout.Header>
       <Layout.Content className="content">
         <Card
