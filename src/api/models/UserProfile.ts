c055022017f3eb7e9b51fb7f2adfc0db25dfb868
Christopher Villamarín <alexpila.314@gmail.com>
2024-03-28 11:52:02 -0500

    feat(users): Update user profile component
    This commit includes the following changes:
    - Added all fields from UserProfile type to ProfileDescriptions component.
    - Separated ChangePassword into its own modal component.
    - Adjusted Avatar size and positioning in the header.
    - Updated layout to position all buttons at the end of the header.

--- src/api/models/UserProfile.ts
+++ src/api/models/UserProfile.ts
@@ -2,11 +2,15 @@ export type UserProfile = {
   uuid: string;
   email: string;
   first_name: string;
-  last_name: string;
+  last_name: null| string;
   is_active: boolean;
   is_staff: boolean;
-  id_card: string | null;
+  id_card: null| string;
   roles: string[];
   parent_accounts: string[];
-  child_accounts: UserProfile[] | null;
+  birth_date: string | null;
+  marital_status: string;
+  education_level: string;
+  home_address: null | string;
+  photo: null | string;
 }
--- src/modules/Dashboard/Profile/Profile.tsx
+++ src/modules/Dashboard/Profile/Profile.tsx
@@ -4,28 +4,31 @@ import { Layout, Card, Spin } from 'antd';
 const { Header, Content, Footer } = Layout;
 
 const ProfileDescriptions = lazy(() => import('./ProfileDescriptions'));
-const ChangePassword = lazy(() => import('./ChangePassword')); // Add this line
+const ChangePasswordModal = lazy(() => import('./ChangePasswordModal')); // Add this line
 
 const Profile: React.FC = () => {
   return (
     <Layout>
-      <Header className="flex top-0 w-full  items-center justify-center print:hidden">
+      <Header className="flex top-0 w-full items-center justify-between print:hidden">
         <h2 className="text-sm sm:text-base md:text-lg lg:text-xl text-white text-opacity-80 text-ellipsis">
           Perfil
         </h2>
+        <>
+          <Suspense fallback={<Spin tip="Cargando datos..." />}>
+            <ChangePasswordModal />
+          </Suspense>
+        </>
       </Header>
+
       <Content>
         <Card
           className="max-w-xs"
           title="Información del perfil"
         >
-          <Suspense fallback={<Spin tip="Cargando datos..." />}>
+          <Suspense fallback={<div>Loading...</div>}>
             <ProfileDescriptions />
           </Suspense>
         </Card>
-        <Suspense fallback={<Spin tip="Cargando datos..." />}>
-          <ChangePassword />
-        </Suspense>
       </Content>
       <Footer style={{ textAlign: 'center' }}>
         WanWayTech © {new Date().getFullYear()}
