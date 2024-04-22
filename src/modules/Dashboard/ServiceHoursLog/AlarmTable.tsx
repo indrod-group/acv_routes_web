462128fd34f94c7274f0019a6a61510a73be7db0
Christopher Villamarín <alexpila.314@gmail.com>
2024-04-22 18:00:46 -0500

    feat(hours-log): add ascend and descend sorter in alarms and devices tables

--- src/modules/Dashboard/ServiceHoursLog/AlarmTable.tsx
+++ src/modules/Dashboard/ServiceHoursLog/AlarmTable.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Table, Pagination, Card } from 'antd';
 import { getAlarmCode } from './ChartUtils';
 import { Alarm } from '../../../api/models';
+import { SortOrder } from 'antd/es/table/interface';
 
 interface AlarmTableProps {
   alarms: Alarm[];
@@ -13,42 +14,56 @@ const columns = [
     title: 'Alarmas',
     dataIndex: 'alarm_code',
     key: 'alarm_code',
-    render: (code: string) => getAlarmCode(code) || "Alarma Desconocida"
+    sorter: (a: Alarm, b: Alarm) => (a.alarm_code as string).localeCompare(b.alarm_code as string),
+    sortDirections: ['descend', 'ascend'] as SortOrder[],
+    render: (code: string) => getAlarmCode(code) || "Alarma Desconocida",
   },
   {
     title: 'Fecha y hora',
     dataIndex: 'time',
     key: 'time',
+    sorter: (a: Alarm, b: Alarm) => a.time - b.time,
+    sortDirections: ['descend', 'ascend'] as SortOrder[],
     render: (time: number) => new Date(time * 1000).toLocaleString()
   },
   {
     title: 'Latitud',
     dataIndex: 'lat',
     key: 'lat',
+    sorter: (a: Alarm, b: Alarm) => (a.lat ?? "").localeCompare(b.lat ?? ""),
+    sortDirections: ['descend', 'ascend'] as SortOrder[],
     render: (lat: string | null) => lat ?? 'N/A'
   },
   {
     title: 'Longitud',
     dataIndex: 'lng',
     key: 'lng',
+    sorter: (a: Alarm, b: Alarm) => (a.lng ?? "").localeCompare(b.lng ?? ""),
+    sortDirections: ['descend', 'ascend'] as SortOrder[],
     render: (lng: string | null) => lng ?? 'N/A'
   },
   {
     title: 'Dirección',
     dataIndex: 'address',
     key: 'address',
+    sorter: (a: Alarm, b: Alarm) => (a.address ?? "").localeCompare(b.address ?? ""),
+    sortDirections: ['descend', 'ascend'] as SortOrder[],
     render: (address: string | null) => address ?? 'N/A'
   },
   {
     title: 'Velocidad',
     dataIndex: 'speed',
     key: 'speed',
+    sorter: (a: Alarm, b: Alarm) => (a.speed ?? 0) - (b.speed ?? 0),
+    sortDirections: ['descend', 'ascend'] as SortOrder[],
     render: (speed: number | null) => speed != null ? `${speed} km/h` : 'N/A'
   },
   {
     title: 'Giro',
     dataIndex: 'course',
     key: 'course',
+    sorter: (a: Alarm, b: Alarm) => (a.course ?? 0) - (b.course ?? 0),
+    sortDirections: ['descend', 'ascend'] as SortOrder[],
     render: (course: number | null) => course != null ? `${course}°` : 'N/A'
   }
 ];
