181985a43c74dc49f05724199503dcdebf98257a
Christopher Villamarín <alexpila.314@gmail.com>
2024-08-09 20:07:36 -0500

    feat: add license's card list

--- src/modules/Dashboard/Profile/License/index.tsx
+++ src/modules/Dashboard/Profile/License/index.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import { Card, Carousel, Image, Divider } from 'antd'; // Importa el componente Carousel
-import { License } from '../../../../api/models';
+import React, { useState } from 'react';
+import { Card, Carousel, Image, Divider, List, Pagination } from 'antd';
 import { useLicenses } from '../../../../api/hooks';
+
+interface LicensePaginationSpanProps {
+  total: number;
+  range: [number, number];
+}
+
+const LicensePaginationSpan: React.FC<LicensePaginationSpanProps> = ({ total, range }) => {
+  return (
+    <span className="px-2 py-1">
+      Mostrando {range[0]} - {range[1]} de {total} licencias
+    </span>
+  );
+}
 
 const LicenseCard: React.FC = () => {
   const { licenses } = useLicenses();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(1);
 
   if (licenses.length === 0) {
     return (
@@ -18,35 +32,72 @@ const LicenseCard: React.FC = () => {
     );
   }
 
-  const license: License = licenses[0];
-  const photos: string[] = [license.front_image, license.back_image];
+  const handlePageChange = (page: number, pageSize?: number) => {
+    setCurrentPage(page);
+    if (pageSize) {
+      setItemsPerPage(pageSize);
+    }
+  };
 
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex: number = startIndex + itemsPerPage;
+  const currentLicense = licenses.slice(startIndex, endIndex).map((license) => ({
+    ...license, key: license.id
+  }));
+  const getPhotos = (index: number) => {
+    return [licenses[index].front_image, licenses[index].back_image];
+  }
   return (
-    <Card
-      className='w-full h-80 flex-grow xl:max-w-xl xs:max-w-xs mt-4 md:mt-0 max-h-100 items-center justify-center'
-      title={`Licencia tipo ${license.type} - ${license.points} puntos`}
-    >
-      <Carousel
-        style={{ height: 250 }}
-        autoplay={false}
-        draggable={true}
-        dots={true}
-        swipeToSlide
-        lazyLoad='progressive'
-      >
-        {photos.map((photo, index) => (
-          <Image.PreviewGroup key={`${index}-${photo}`} items={photos}>
-            <Image
-              src={photo}
-              height={250}
-              alt={`${index}-${photo}`}
-            />
-          </Image.PreviewGroup>
-        ))}
-      </Carousel>
-      <Divider />
-      <p>Válida desde {license.issue_date} hasta {license.expiry_date}.</p>
-    </Card>
+    <List
+      className="w-full"
+      header={
+        <>
+          {licenses.length === 0 || licenses.length === 1 ? <></> : <h2 className='flex justify-center items-center'>Lista de licencias</h2>
+          }
+          <Pagination
+            className="mt-4 mb-4 mx-auto"
+            total={licenses.length}
+            current={currentPage}
+            onChange={handlePageChange}
+            defaultPageSize={itemsPerPage}
+            hideOnSinglePage
+            showTotal={(total, range) => <LicensePaginationSpan total={total} range={range} />}
+          />
+        </>
+      }
+      itemLayout="horizontal"
+      dataSource={currentLicense}
+      renderItem={(item, index) => (
+        <List.Item>
+          <Card
+            className='w-full h-90 flex-grow xl:max-w-xl xs:max-w-xs mt-4 md:mt-0 max-h-100 items-center justify-center'
+            title={`Licencia tipo ${item.type} - ${item.points} puntos`}
+          >
+            <Carousel
+              style={{ height: 250 }}
+              autoplay={false}
+              draggable={true}
+              dots={true}
+              swipeToSlide
+              lazyLoad='progressive'
+            >
+              {getPhotos(index).map((photo, i) => (
+                <Image.PreviewGroup key={`${i}-${photo}`} items={getPhotos(index)}>
+                  <Image
+                    src={photo}
+                    height={250}
+                    alt={`${i}-${photo}`}
+                  />
+                </Image.PreviewGroup>
+              ))}
+            </Carousel>
+            <Divider />
+            <p>Válida desde {item.issue_date} hasta {item.expiry_date}.</p>
+          </Card>
+        </List.Item>
+      )}
+    />
+
   );
 };
 
